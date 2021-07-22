cbdb25724d4f54d394e994f343f34f656c594d8e
Rahul Dahal <rdaahal@gmail.com>
2021-07-22 16:19:30 +0545

    initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => res.status(200).json({ message: "Hello from /" }));
+
+export default app;
---
+++ backend/src/init.js
@@ -0,0 +1,5 @@
+import app from "./app";
+
+const port = 3000;
+
+app.listen(port, () => console.log(`The server is listening on port ${port}`));
