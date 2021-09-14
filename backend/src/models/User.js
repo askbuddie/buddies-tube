bd368b3dfe1369f50f7d6fdb92b272a5af4ff240
Ravi Lamichhane <ravilamichhane30@gmail.com>
2021-09-14 23:22:43 +0545

    feat:added email validation

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -58,6 +58,25 @@ userSchema.methods.validatePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
+
+
+userSchema.path('email').validate(function (value) {
+  const emailregx =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  console.log(1);
+  return emailregx.test(value);
+}, 'Email must be valid');
+
+
+// This will run after the email is validated and will check if the email is already in use
+userSchema.path('email').validate(async function (value) {
+  // this.constructor is the model we are using
+  const user = await this.constructor.findOne({ email: value });
+  console.log(2);
+  return !user;
+}, 'Email already exists');
+
+
 const User = new model('User', userSchema);
 
 export default User;
