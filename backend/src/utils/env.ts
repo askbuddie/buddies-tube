31aee362bbf535a1670cdfcf7700aa58badd694a
magusikrak <sugamkarki7058@gmail.com>
2021-10-17 14:01:31 +0545

    typechecking for port and dbURI

--- backend/src/utils/env.ts
+++ backend/src/utils/env.ts
@@ -3,8 +3,8 @@ import { config } from 'dotenv';
 config();
 
 const envs = {
-  port: process.env.PORT,
-  dbURI: process.env.DB_URI,
+  port: process.env.PORT ?? '3000',
+  dbURI: process.env.DB_URI ?? 'mongo://localhost/buddie-tube',
 };
 
 export default envs;
